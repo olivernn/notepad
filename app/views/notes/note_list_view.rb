cc79e1cc58cac6b670250c99e79e60f39397521b
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-01-26 13:01:07 +0000

    instance variables defined in the controller should be available in the mustache templates

--- app/views/notes/note_list_view.rb
+++ app/views/notes/note_list_view.rb
@@ -1,7 +1,5 @@
 module Notes
   class NoteListView < MustacheRails
-    def notes
-      @view_context.instance_variable_get("@notes")
-    end
+
   end
 end
--- config/initializers/mustache_handler.rb
+++ config/initializers/mustache_handler.rb
@@ -1,11 +1,11 @@
 class MustacheRails < Mustache
-  
+
   def initialize(view_context, template_source)
     @view_context = view_context
     self.template = template_source
     assign_variables!
   end
-    
+
   def respond_to?(method_sym, include_private = false)
     if @view_context.respond_to?(method_sym) 
       true
@@ -13,19 +13,20 @@ class MustacheRails < Mustache
       super
     end
   end
-  
+
   def method_missing(method_name, *args, &block)
     @view_context.send(method_name,*args, &block)
   end
-  
-  # we wish to add instance vars from the view_context only 
-  # none funcky ones
+
   private
   def assign_variables!
     variables = @view_context.instance_variable_names.select{|name| name =~ /^@[^_]/}
-    variables.each do |name| 
-      assign_name = name.gsub(/@/, "_")
-      self[assign_name.to_sym] = @view_context.instance_variable_get(name)
+    variables.each do |name|
+      singleton_class.class_eval do
+        define_method name.tr('@','') do
+          @view_context.instance_variable_get(name)
+        end
+      end
     end
   end
 end
